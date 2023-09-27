9f1cb9bae0332846ec38b31e7685bd92116c0062
Raghavender Singh <92844911+RaghavenderSingh@users.noreply.github.com>
2023-09-27 14:36:46 +0530

    Update app_constants.dart

--- lib/utils/constants/app_constants.dart
+++ lib/utils/constants/app_constants.dart
@@ -2,5 +2,5 @@
 class AppConstants {
   static const String mobileConnectionStatus = "ConnectivityResult.mobile";
   static const String wifiConnectionStatus = "ConnectivityResult.wifi";
-  static const String agoraAppId = "<Your Agora APP Id>";
+  static const String agoraAppId = "afca5c33004245d5b3b28c618714cb5c";
 }
